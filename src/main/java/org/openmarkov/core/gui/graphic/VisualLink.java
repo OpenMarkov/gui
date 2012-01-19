a226600943d8370fb877847952fdb460c22708d4
jesus_oliva <jesus_oliva@localhost>
2012-01-19 18:37:21 +0100

    Painting links bug correction

--- src/main/java/org/openmarkov/core/gui/graphic/VisualLink.java
+++ src/main/java/org/openmarkov/core/gui/graphic/VisualLink.java
@@ -503,14 +503,15 @@ public class VisualLink extends VisualElement {
 			}
 			sPoint = source.cutPoint(line, g);
 			ePoint = destination.cutPoint(line, g);
-			if ((sPoint != null) && (ePoint != null) && (link.isDirected())) {
+			if ((sPoint != null) && (ePoint != null) && (link != null) && 
+					(link.isDirected())) {
 				paintArrow(g, sPoint, ePoint);
 			}
-			else if ((sPoint != null) && (ePoint != null) && 
+			else if ((sPoint != null) && (ePoint != null) && (link != null) &&
 					(!link.isDirected())) {
 				paintLine(g, sPoint, ePoint);
 			}
-		} else if (link.isDirected()){
+		} else if ((link==null) || (link.isDirected())){
 			//Paint the arrow while the user has not released the button of the mouse
 			paintArrow(g, startPoint, endPoint);			
 		}
