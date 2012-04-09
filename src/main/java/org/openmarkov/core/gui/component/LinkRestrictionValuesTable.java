b51b4d99490de068923a7b036027f378db68dbeb
carolinekonig <carolinekonig@localhost>
2012-04-09 20:55:59 +0000

    removed registering as listener to PNSupport for LinkRestrictionValuesTable

--- src/main/java/org/openmarkov/core/gui/component/LinkRestrictionValuesTable.java
+++ src/main/java/org/openmarkov/core/gui/component/LinkRestrictionValuesTable.java
@@ -68,8 +68,6 @@ public class LinkRestrictionValuesTable extends ValuesTable implements
 		node1 = (ProbNode) link.getNode1().getObject();
 		node2 = (ProbNode) link.getNode2().getObject();
 		net = node1.getProbNet();
-		net.getPNESupport().addUndoableEditListener(this);
-
 	}
 
 	/**
@@ -100,7 +98,8 @@ public class LinkRestrictionValuesTable extends ValuesTable implements
 				int variable1Index = col - 1;
 				int variable2Index = node2.getVariable().getNumStates() - row;
 				if ((Integer) newValue == 0) {
-					if (!node2.getPotentials().isEmpty()&& node2.getPotentials().get(0).getPotentialType() == PotentialType.TABLE) {
+					if (!node2.getPotentials().isEmpty()
+							&& node2.getPotentials().get(0).getPotentialType() == PotentialType.TABLE) {
 						Potential potential = LinkRestrictionPotentialOperations
 								.updatePotentialByAddLinkRestriction(node2,
 										(TablePotential) link
