d07dc9119057d33fc19cf27e65e7d828f351d26c
maryebra <maryebra@localhost>
2012-07-20 13:10:45 +0200

    Bugfix: #8 Standard domains.

--- src/main/java/org/openmarkov/core/gui/dialog/node/StandarDomainPanel.java
+++ src/main/java/org/openmarkov/core/gui/dialog/node/StandarDomainPanel.java
@@ -12,6 +12,8 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
 
+import org.openmarkov.core.gui.localize.StringResource;
+import org.openmarkov.core.gui.localize.StringResourceLoader;
 import org.openmarkov.core.gui.util.GUIDefaultStates;
 import org.openmarkov.core.model.network.State;
 
@@ -26,7 +28,7 @@ public class StandarDomainPanel extends JPanel implements ItemListener{
 	
 	private  ButtonGroup buttonGroup = new ButtonGroup();
 	private ArrayList<JRadioButton> radioButtons = new ArrayList<JRadioButton>();
-	
+	private StringResource selectableStringResource;
 	public StandarDomainPanel() {
 		initialize();
 		repaint();
@@ -35,8 +37,15 @@ public class StandarDomainPanel extends JPanel implements ItemListener{
 	 public void initialize() {
 		 setLayout (new BoxLayout(this, BoxLayout.Y_AXIS));
 		// ButtonGroup buttonGroup = new ButtonGroup();
-		 String  [] defaultStates = GUIDefaultStates.getListStrings();
-		
+		 selectableStringResource =
+		            StringResourceLoader.getUniqueInstance().getBundleSelectables();
+		 String [] defaultStates = {selectableStringResource.getString("defaultStates.absentPresent.Text"),
+				 selectableStringResource.getString("defaultStates.noYes.Text"),
+				 selectableStringResource.getString("defaultStates.negativePositive.Text"),
+				 selectableStringResource.getString("defaultStates.absentMildModerateSevere.Text"),
+				 selectableStringResource.getString("defaultStates.lowMediumhigh.Text")};
+		 //String  [] defaultStates = GUIDefaultStates.getListStrings();
+		// String  [] defaultStates2 = GUIDefaultStates.getStringsLanguageDependent(GUIDefaultStates.getListStrings());
 		for (String defaultState : defaultStates) {
 			
 			JRadioButton radioButton = new JRadioButton (defaultState);
