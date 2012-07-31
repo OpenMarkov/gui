702d68640f22f0e9aeb22b73d6ca773e7c9e7601
mluque <mluque@localhost>
2012-07-31 14:18:38 +0200

    Adding a test about the expansion of the SMM.

--- src/test/java/org/openmarkov/core/gui/dialog/costeffectiveness/FactoryExpandedSMMTest.java
+++ src/test/java/org/openmarkov/core/gui/dialog/costeffectiveness/FactoryExpandedSMMTest.java
@@ -10,17 +10,69 @@ package org.openmarkov.core.gui.dialog.costeffectiveness;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+import org.openmarkov.core.exception.NotEnoughMemoryException;
+import org.openmarkov.core.model.network.NetsFactory;
+import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.core.model.network.potential.Potential;
+import org.openmarkov.core.model.network.potential.PotentialRole;
+import org.openmarkov.core.model.network.potential.TablePotential;
+import org.openmarkov.core.model.network.potential.TablePotentialTest;
+import org.openmarkov.core.model.network.potential.operation.DiscretePotentialOperations;
 
 /**
  * @author manolo
  *
  */
 public class FactoryExpandedSMMTest {
+	/**
+	 * Maximum error allowed in tests. It could be modified by subclasses
+	 * if it is necessary (for example, approximate inference methods).
+	 */
+	protected double maxError = 1E-6;
 
-	@Test
+	
+
+	/*@Test
 	public void test() {
 		fail("Not yet implemented");
+	}*/
+	
+	
+	//@Test
+	public void testExpansionSimpleSMM() {
+
+		ProbNet network = NetsFactory.createSMMWithoutStateVariable();
+		double discount = 1.0;
+		FactoryExpandedSMM expandedNetFactory = null;
+		try {
+			expandedNetFactory = new FactoryExpandedSMM(network, 1, null, 200.0);
+			expandedNetFactory.applyDiscountToUtilityNodes(discount);
+		} catch (NotEnoughMemoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ProbNet expandedNetwork = expandedNetFactory.getExtendedNet();
+		ArrayList<Potential> utilityPotentials = expandedNetwork
+				.getPotentialsRole(PotentialRole.UTILITY);
+
+		TablePotential globalPotential = null;
+		try {
+			globalPotential = DiscretePotentialOperations
+					.sum(utilityPotentials);
+		} catch (NotEnoughMemoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		TablePotentialTest.checkEqualPotentials(globalPotential,globalPotential,maxError);
+
 	}
+
+	
+	
 
 }
