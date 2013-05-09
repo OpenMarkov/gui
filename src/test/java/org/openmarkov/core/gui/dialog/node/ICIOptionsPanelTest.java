81bbfe9f4a766106141f75899573547d2e819539
ibermejo <ibermejo@localhost>
2013-05-09 16:44:06 +0100

    Removes unwanted .svn files

--- src/test/java/org/openmarkov/core/gui/dialog/node/ICIOptionsPanelTest.java
+++ src/test/java/org/openmarkov/core/gui/dialog/node/ICIOptionsPanelTest.java
@@ -10,7 +10,6 @@
 package org.openmarkov.core.gui.dialog.node;
 
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
