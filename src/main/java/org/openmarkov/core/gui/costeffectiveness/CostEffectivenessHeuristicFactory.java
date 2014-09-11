4ba36622bf43d6d728aa4804ef10fafed821028f
ibermejo <ibermejo@localhost>
2014-09-11 18:12:43 +0200

    Use TimeSliceElimination for CEA, as it works better than SimpleElimination

--- src/main/java/org/openmarkov/core/gui/costeffectiveness/CostEffectivenessHeuristicFactory.java
+++ src/main/java/org/openmarkov/core/gui/costeffectiveness/CostEffectivenessHeuristicFactory.java
@@ -6,13 +6,13 @@ import org.openmarkov.core.inference.heuristic.EliminationHeuristic;
 import org.openmarkov.core.inference.heuristic.HeuristicFactory;
 import org.openmarkov.core.model.network.ProbNet;
 import org.openmarkov.core.model.network.Variable;
-import org.openmarkov.inference.heuristic.simpleElimination.SimpleElimination;
+import org.openmarkov.inference.heuristic.timeSliceElimination.TimeSliceElimination;
 
 public class CostEffectivenessHeuristicFactory implements HeuristicFactory {
 
 	@Override
 	public EliminationHeuristic getHeuristic(ProbNet probNet, List<List<Variable>> variables) {
-		return new SimpleElimination(probNet, variables);
+		return new TimeSliceElimination(probNet, variables);
 	}
 
 }
