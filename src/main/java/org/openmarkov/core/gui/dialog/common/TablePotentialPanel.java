43116343d5765df22f1f3dc19d403689ecf2143b
jorgepmartin <jorgepmartin@localhost>
2014-10-29 22:59:47 +0100

    Solved issue #236: https://bitbucket.org/cisiad/org.openmarkov.issues/issue/236/invocationtargetexception-when-trying-to

--- src/main/java/org/openmarkov/core/gui/dialog/common/TablePotentialPanel.java
+++ src/main/java/org/openmarkov/core/gui/dialog/common/TablePotentialPanel.java
@@ -730,18 +730,25 @@ public class TablePotentialPanel extends ProbabilityTablePanel {
     	// We use a temporal value to make the column 1 as the first (column 0)
     	int temp = column-1;
     	
-    	// In this code we get the coordinates (states index) of the variable and
-    	// we calculate the position in the list of potentials. The position
-    	// is the product of each state index and the respective offset
-    	// s[0]*offset[0] + s[1]*offset[1] + ..... + s[n]*offset[n]
-    	for(int i = tablePotential.getDimensions().length - 1; i > 0;i--){
-    		// Dimension of the first parent
-    		int dimension = tablePotential.getDimensions()[i];
+    	// If TablePotential.dimensions  is equals null, the node does not have parents
+    	// and only has one state (we have a utility node without parents)
+    	if(tablePotential.getDimensions() != null){
+        	// In this code we get the coordinates (states index) of the variable and
+        	// we calculate the position in the list of potentials. The position
+        	// is the product of each state index and the respective offset
+        	// s[0]*offset[0] + s[1]*offset[1] + ..... + s[n]*offset[n]
+        	for(int i = tablePotential.getDimensions().length - 1; i > 0;i--){
+        		// Dimension of the first parent
+        		int dimension = tablePotential.getDimensions()[i];
 
-    		// In each iteration this code add the s[i]*offset[i] to the
-    		// position
-    		position += (temp%dimension)*tablePotential.getOffsets()[i]; 
-    		temp = temp/dimension;    
+        		// In each iteration this code add the s[i]*offset[i] to the
+        		// position
+        		position += (temp%dimension)*tablePotential.getOffsets()[i]; 
+        		temp = temp/dimension;    
+        	}
+
+    	}else{
+    		position = 0;
     	}
     	return position;
     }
