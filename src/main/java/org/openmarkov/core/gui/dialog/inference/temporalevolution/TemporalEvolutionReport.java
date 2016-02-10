d0d38285073fa1c6f208217469696754ae4d8198
Jorge P?rez <jorgepmartin@gmail.com>
2016-02-10 16:51:19 +0000

    Created new branch AugmentedBayesianNetwork

---
+++ src/main/java/org/openmarkov/core/gui/dialog/inference/temporalevolution/TemporalEvolutionReport.java
@@ -0,0 +1,123 @@
+package org.openmarkov.core.gui.dialog.inference.temporalevolution;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.openmarkov.core.model.network.Node;
+import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.core.model.network.Variable;
+import org.openmarkov.core.model.network.potential.TablePotential;
+
+import javax.swing.*;
+
+public class TemporalEvolutionReport {
+
+	// Methods
+    /**
+     * creates a new book with temporal evolution of a variable
+     * @throws IOException 
+     */
+//    public void write (String filename,
+//                                                    Map<Variable, TablePotential> temporalEvolution,
+//                                                    ProbNet expandedNetwork,
+//                                                    int numSlices,
+//                                                    Variable variableOfInterest) throws IOException
+//    {
+//        HSSFWorkbook hwb = new HSSFWorkbook ();
+//        String sheetName = variableOfInterest.getBaseName ().toString ();
+//        HSSFSheet sheetTable = hwb.createSheet (sheetName);
+//        // first row, column names
+//        HSSFRow rowIndexes = sheetTable.createRow (0);
+//        rowIndexes.createCell (0).setCellValue ("");
+//        String basename = variableOfInterest.getBaseName ();
+//        List<Node> nodes = expandedNetwork.getNodes ();
+//        List<Node> interestNodes = new ArrayList<> ();
+//        for (int i = 0; i < nodes.size (); i++)
+//        {
+//            if (nodes.get (i).getVariable ().getBaseName ().equals (basename))
+//            {
+//                interestNodes.add (nodes.get (i));
+//            }
+//        }
+//        for (int i = 0; i < interestNodes.size (); i++)
+//        {
+//            rowIndexes.createCell (i + 1).setCellValue (interestNodes.get (i).getVariable ().getTimeSlice());
+//        }
+//        // first column
+//        for (int i = 0; i < variableOfInterest.getNumStates (); i++)
+//        {
+//            HSSFRow row = sheetTable.createRow (i + 1);
+//            row.createCell (0).setCellValue (variableOfInterest.getStateName (i));
+//        }
+//        for (int i = 0; i < variableOfInterest.getNumStates (); i++)
+//        {
+//            for (int j = 0; j <= numSlices; j++)
+//            {
+//                String basenameInterest = variableOfInterest.getBaseName ();
+//                List<Node> expandedNodes = expandedNetwork.getNodes ();
+//                for (int k = 0; k < expandedNodes.size (); k++)
+//                {
+//                    if (expandedNodes.get (k).getVariable ().getBaseName ().equals (basenameInterest)
+//                        && expandedNodes.get (k).getVariable ().getTimeSlice () == j)
+//                    {
+//                        double value = temporalEvolution.get (expandedNodes.get (k).getVariable ()).getValues ()[i];
+//                        // cell(row, column) = cell(i+1, j+1)
+//                        sheetTable.getRow (i + 1).createCell (j + 1).setCellValue (value);
+//                    }
+//                }
+//            }
+//        }
+//        String targetFilename =  filename.endsWith (".xls")? filename : filename +".xls";
+//        FileOutputStream fileOut = new FileOutputStream (targetFilename);
+//        hwb.write (fileOut);
+//        fileOut.close ();
+//    }
+
+
+    // Methods
+    /**
+     * creates a new book with temporal evolution of a variable
+     * @throws IOException
+     */
+    public void write (String filename,
+                       JTable jtable) throws IOException
+    {
+        HSSFWorkbook hwb = new HSSFWorkbook ();
+        String sheetName = filename;
+        HSSFSheet sheetTable = hwb.createSheet ("Temporal Evolution Report");
+        // first row, column names
+        HSSFRow rowIndexes = sheetTable.createRow (0);
+        rowIndexes.createCell (0).setCellValue ("");
+
+        for (int i = 1; i < jtable.getColumnCount(); i++)
+        {
+            rowIndexes.createCell (i + 1).setCellValue (jtable.getColumnModel().getColumn(i).getHeaderValue().toString());
+        }
+        // fill data
+        for (int i = 0; i < jtable.getRowCount(); i++)
+        {
+            HSSFRow row = sheetTable.createRow (i + 1);
+            for (int j = 0; j < jtable.getColumnCount(); j++) {
+                if(jtable.getValueAt(i, j) instanceof String) {
+                    row.createCell(j).setCellValue((String) jtable.getValueAt(i, j));
+                } else if (jtable.getValueAt(i, j) instanceof Integer) {
+                    row.createCell(j).setCellValue((Integer) jtable.getValueAt(i, j));
+                }else {
+                    row.createCell(j).setCellValue((Double) jtable.getValueAt(i, j));
+                }
+            }
+
+        }
+
+        String targetFilename =  filename.endsWith (".xls")? filename : filename +".xls";
+        FileOutputStream fileOut = new FileOutputStream (targetFilename);
+        hwb.write (fileOut);
+        fileOut.close ();
+    }
+}
---
+++ src/test/java/org/openmarkov/core/gui/dialog/common/TablePotentialPanelTest.java
@@ -0,0 +1,128 @@
+package org.openmarkov.core.gui.dialog.common;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openmarkov.core.exception.NodeNotFoundException;
+import org.openmarkov.core.model.network.EvidenceCase;
+import org.openmarkov.core.model.network.Finding;
+import org.openmarkov.core.model.network.Node;
+import org.openmarkov.core.model.network.NodeType;
+import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.core.model.network.Variable;
+import org.openmarkov.core.model.network.potential.PotentialRole;
+import org.openmarkov.core.model.network.potential.TablePotential;
+import org.openmarkov.core.model.network.potential.UniformPotential;
+import org.openmarkov.core.model.network.type.BayesianNetworkType;
+
+public class TablePotentialPanelTest extends TablePotentialPanel{
+
+	public TablePotentialPanelTest() throws NodeNotFoundException {
+		super(probNet.getNode("E"));
+	}
+
+	private static ProbNet probNet;
+	@BeforeClass
+	public static void setUp() throws Exception {
+		probNet = buildpotential_panel_reordered_pgmx();
+	}
+
+	@Test
+	public void testHowManyRows() throws NodeNotFoundException{
+		assertEquals(this.howManyRows(probNet.getNode("E")),5); 
+	}
+	
+	@Test
+	public void testNumberOfColumns(){
+		assertTrue(this.columns.length == 17);
+	}
+	
+	@Test
+	public void testGetEvidenceCaseFromSelectedColumn(){
+		this.selectedColumn = 3;
+		EvidenceCase ec = this.getEvidenceCaseFromSelectedColumn();
+		String i = null;
+		for(Finding f : ec.getFindings()){
+			i = f.getVariable().getName();
+			switch(i){
+			case "A":
+				assertTrue(f.getStateIndex() == 0);
+				break;
+			case "B":
+				assertTrue(f.getStateIndex() == 1);
+				break;
+			case "C":
+				assertTrue(f.getStateIndex() == 0);
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void testGetEvidenceCaseFromSelectedColumn2(){
+		this.selectedColumn = 9;
+		EvidenceCase ec = this.getEvidenceCaseFromSelectedColumn();
+		
+		String i = null;
+		
+		for(Finding f : ec.getFindings()){
+			i = f.getVariable().getName();
+			switch(i){
+			case "A":
+				assertTrue(f.getStateIndex() == 2);
+				break;
+			case "B":
+				assertTrue(f.getStateIndex() == 0);
+				break;
+			case "C":
+				assertTrue(f.getStateIndex() == 0);
+				break;
+			}
+		}
+	}
+	
+
+	private static ProbNet buildpotential_panel_reordered_pgmx () {
+		  ProbNet probNet = new ProbNet(BayesianNetworkType.getUniqueInstance());
+		  // Variables
+		  Variable varA = new Variable("A", "absent", "mild", "moderate", "severe");
+		  Variable varB = new Variable("B", "no", "yes");
+		  Variable varC = new Variable("C", "negative", "positive");
+		  Variable varE = new Variable("E", "absent", "present");
+
+		  // Nodes
+		  Node nodeA= probNet.addNode(varA, NodeType.CHANCE);
+		  Node nodeB= probNet.addNode(varB, NodeType.CHANCE);
+		  Node nodeC= probNet.addNode(varC, NodeType.CHANCE);
+		  Node nodeE= probNet.addNode(varE, NodeType.CHANCE);
+
+		  // Links
+		  probNet.makeLinksExplicit(false);
+		  probNet.addLink(nodeA, nodeE, true);
+		  probNet.addLink(nodeB, nodeE, true);
+		  probNet.addLink(nodeC, nodeE, true);
+
+		  // Potentials
+		  UniformPotential potA = new UniformPotential(Arrays.asList(varA), PotentialRole.CONDITIONAL_PROBABILITY);
+		  nodeA.setPotential(potA);
+
+		  UniformPotential potB = new UniformPotential(Arrays.asList(varB), PotentialRole.CONDITIONAL_PROBABILITY);
+		  nodeB.setPotential(potB);
+
+		  UniformPotential potC = new UniformPotential(Arrays.asList(varC), PotentialRole.CONDITIONAL_PROBABILITY);
+		  nodeC.setPotential(potC);
+
+		  TablePotential potE = new TablePotential(Arrays.asList(varE, varA, varB, varC), PotentialRole.CONDITIONAL_PROBABILITY);
+		  potE.values = new double[]{0.9, 0.1, 0.8, 0.2, 0.7, 0.3, 0.6, 0.4, 0.5, 0.5, 0.4, 0.6, 0.3, 0.7, 0.2, 0.8, 0.001, 0.999, 0, 1, 0.9, 0.1, 0.8, 0.2, 0.7, 0.3, 0.6, 0.4, 0.5, 0.5, 0.4, 0.6};
+		  nodeE.setPotential(potE);
+
+		  // Link restrictions and revealing states
+		  // Always observed nodes
+
+		 return probNet;
+		}
+}
