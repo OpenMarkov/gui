8fa7669f34c322abf2dcd16b8465c5c296a28737
carmenyago <carmenyago@localhost>
2016-06-19 20:28:16 +0200

    fillDistributionsTableModel method commented for compiling purposes. It has to be changed.  The changes are between //CMI //CMF tags

--- src/main/java/org/openmarkov/core/gui/dialog/node/UncertainValuesDialog.java
+++ src/main/java/org/openmarkov/core/gui/dialog/node/UncertainValuesDialog.java
@@ -177,9 +177,19 @@ public class UncertainValuesDialog extends OkCancelHorizontalDialog {
     public UncertainValuesDialog(Window owner, EvidenceCase configuration, TablePotential potential)
             throws WrongCriterionException {
         super(owner);
+        //CMI Setting isChanceVariable=true for compiling
+        isChanceVariable= true;
+        /*
         isChanceVariable = !(potential.isUtility());
+        */
+        //CMF
         distributionTypes = new ArrayList<>();
+        //CMI Now variable is always potential.getVariable(0);
+        /*
         variable = isChanceVariable ? potential.getVariable(0) : potential.getUtilityVariable();
+        */
+        variable = potential.getVariable(0);
+        //CMF
         setTitle(getConfigurationDescription(variable, isChanceVariable, configuration));
         posBase = getPositionBaseUncertainValue(potential, configuration);
         setResizable(true);
@@ -259,6 +269,11 @@ public class UncertainValuesDialog extends OkCancelHorizontalDialog {
             EvidenceCase configuration,
             TablePotential potential)
             throws WrongCriterionException {
+    	//CMI
+    	return;
+    	//CMF
+    	//CMI
+    	/*
         UncertainValue[] uncertainTable = potential.getUncertaintyTable();
         TablePotential projectedPotential = null;
         try {
@@ -307,6 +322,8 @@ public class UncertainValuesDialog extends OkCancelHorizontalDialog {
         TableColumn column = columnModel.getColumn(DISTRIBUTION_COLUMN_INDEX);
         column.setCellEditor(new DefaultCellEditor(distributionTypesCombo));
         columnModel.getColumn(0).setCellEditor(null);
+        */
+    	//CMF
     }
 
     private String getString(double[] parameters) {
