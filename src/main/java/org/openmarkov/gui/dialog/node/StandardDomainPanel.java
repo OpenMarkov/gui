8a90f57cce10376e07d3f936f151e06af3755b2f
Jorge Pérez <jorgepmartin@gmail.com>
2018-02-26 10:56:26 +0100

    Modifying and including new license of OpenMarkov

--- src/main/java/org/openmarkov/gui/dialog/node/StandardDomainPanel.java
+++ src/main/java/org/openmarkov/gui/dialog/node/StandardDomainPanel.java
@@ -1,4 +1,11 @@
 
+/*
+ * Copyright (c) CISIAD, UNED, Spain,  2018. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
+
 package org.openmarkov.gui.dialog.node;
 
 import java.awt.BorderLayout;
