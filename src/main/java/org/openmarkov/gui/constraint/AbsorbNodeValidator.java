51475a14b8b093efcc4f84b2a02e9d576b4a149c
Iago París <iagoparis@dia.uned.es>
2018-07-10 15:10:20 +0200

    AbsorbNodeEdit now merge utility children into one

--- src/main/java/org/openmarkov/gui/constraint/AbsorbNodeValidator.java
+++ src/main/java/org/openmarkov/gui/constraint/AbsorbNodeValidator.java
@@ -8,12 +8,28 @@ import java.util.List;
 public class AbsorbNodeValidator {
     // The node is of decision or chance and has only a utility child
     public static boolean validate(Node node) {
-        boolean isDecisionOrChance = node.getNodeType() == NodeType.CHANCE || node.getNodeType() == NodeType.DECISION;
-        List<Node> children = node.getChildren();
 
-        // If no children, second expression is false and so third expression is not evaluated,
-        // avoiding IndexOutOfBound on children.get(0).
-        return (isDecisionOrChance && children.size() == 1 && children.get(0).getNodeType() == NodeType.UTILITY);
+        // Test if decision or chance
+        boolean isDecisionOrChance = node.getNodeType() == NodeType.CHANCE || node.getNodeType() == NodeType.DECISION;
+        if (!isDecisionOrChance) {
+            return false;
+        }
+
+        // Test if only utility children and no grandchildren
+        List<Node> children = node.getChildren();
+        switch (children.size()) {
+            case 0:
+                return false;
+            default:
+                for (Node child : children) {
+                    if (child.getNodeType() != NodeType.UTILITY || child.getChildren().size() > 0) {
+                        return false;
+                    }
+                }
+                // Only utility children, no grandchildren
+                return true;
+
+        }
 
     }
 }
