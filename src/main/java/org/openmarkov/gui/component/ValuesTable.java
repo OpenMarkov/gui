5e1e26aa5f48aa95e6474048a369fd7716959b51
Jorge Pérez <jorgepmartin@gmail.com>
2019-01-14 10:08:14 +0100

    Allow row and column selection

--- src/main/java/org/openmarkov/gui/component/ValuesTable.java
+++ src/main/java/org/openmarkov/gui/component/ValuesTable.java
@@ -307,8 +307,8 @@ public class ValuesTable extends KeyTable implements PNUndoableEditListener {
 		setFirstColumnHidden(false); // key prefix column is hidden
 		setShowColumnHeader(false); // no column header here
 		setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		setRowSelectionAllowed(false);
-		setColumnSelectionAllowed(false);
+		setRowSelectionAllowed(true);
+		setColumnSelectionAllowed(true);
 		setGridColor(Color.DARK_GRAY);
 		setDefaultRenderer(Double.class, new ValuesTableCellRenderer(0));
 		setDefaultRenderer(String.class, new ValuesTableCellRenderer(0));
