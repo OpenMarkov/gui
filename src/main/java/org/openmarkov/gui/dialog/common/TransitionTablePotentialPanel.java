3111c4aaa9217c1a3a2f16bd8cdd136b8735ab95
Carmen María Yago Sánchez <carmen.yago@gmail.com>
2019-12-16 09:06:21 +0100

    Panel for TransitionTablePotential. Previously that potential used TableWithEventsPotentialPanel. Now it has a TableWithEventsPanel (formerly named TableWithEventsPotentialPânel)

---
+++ src/main/java/org/openmarkov/gui/dialog/common/TransitionTablePotentialPanel.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) CISIAD, UNED, Spain,  2019. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
+
+package org.openmarkov.gui.dialog.common;
+
+import org.openmarkov.core.model.network.Node;
+import org.openmarkov.core.model.network.potential.TableWithEvents;
+
+/**
+ * TransitionTablePotentialPanel for TranssitionPotential
+ *
+ * @version 1.0 - cyago - 24/03/2019 - only one TableWithEventsPanel
+ */
+@SuppressWarnings("serial") @PotentialPanelPlugin( potentialType = "TransitionTable")
+public class TransitionTablePotentialPanel
+		extends TableWithEventsPanel {
+
+	public TransitionTablePotentialPanel(Node node) {
+		super(node, (TableWithEvents) node.getPotentials().get(0));
+
+	}
+
+
+}
+
+
+
+
