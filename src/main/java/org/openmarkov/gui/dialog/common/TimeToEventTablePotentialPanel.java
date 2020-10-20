f91249c08bed3cbef0418287a62cde6361987a86
Carmen María Yago Sánchez <carmen.yago@gmail.com>
2020-10-20 15:44:13 +0200

    TimeToEventTablePotentialPanel: parametrized probabilistic distributions added

--- src/main/java/org/openmarkov/gui/dialog/common/TimeToEventTablePotentialPanel.java
+++ src/main/java/org/openmarkov/gui/dialog/common/TimeToEventTablePotentialPanel.java
@@ -9,7 +9,7 @@ package org.openmarkov.gui.dialog.common;
 
 import org.openmarkov.core.model.network.*;
 import org.openmarkov.core.model.network.modelUncertainty.ProbDensFunction;
-import org.openmarkov.core.model.network.modelUncertainty.ProbDensFunctionManager;
+import org.openmarkov.core.model.network.modelUncertainty.ParametrizedFunction.ParametrizedFunctionManager;
 import org.openmarkov.core.model.network.potential.TablePotential;
 import org.openmarkov.core.model.network.potential.TableWithEvents;
 import org.openmarkov.core.model.network.potential.TimeToEventTablePotential;
@@ -18,6 +18,10 @@ import org.openmarkov.gui.component.PotentialsTablePanelOperations;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
 /**
  * This class implements a Table eventTablePotential table.
@@ -28,9 +32,11 @@ import java.awt.event.*;
 public class TimeToEventTablePotentialPanel
 		extends ProbabilityTablePanel implements ItemListener{
 
-	private JPanel jDistribution;
+	private JPanel jDistributionAndParametrization;
 	private JLabel jlDistribution;
 	private JComboBox<String> jcDistribution;
+	private JLabel jlParametrization;
+	private JComboBox<String> jcParametrization;
 	private TableWithEventsPanel tableWithEventsPanel;
 
 	protected Node node;
@@ -38,8 +44,11 @@ public class TimeToEventTablePotentialPanel
 	protected TimeToEventTablePotential tteTablePotential;
 	protected TablePotential tablePotential;
 	protected TableWithEvents tableWithEvents;
-	protected ProbDensFunctionManager distributionManager;
+	protected ParametrizedFunctionManager parametrizedFunctionManager;
+
+//	protected ProbDensFunctionManager distributionManager;
 	protected ProbDensFunction distribution;
+
 
 
 	public TimeToEventTablePotentialPanel(Node node)
@@ -54,8 +63,9 @@ public class TimeToEventTablePotentialPanel
         tableWithEvents =  tteTablePotential.getTableWithEvents();
 
 
+
         setLayout(new BorderLayout());
-		this.add(getjDistribution(),BorderLayout.PAGE_START);
+		this.add(getjDistributionAndParametrization(),BorderLayout.PAGE_START);
 
 
 		this.add( getTableWithEventsPanel(),BorderLayout.CENTER);
@@ -85,37 +95,78 @@ public class TimeToEventTablePotentialPanel
 		}
 		return jlDistribution;
 	}
-	protected JPanel getjDistribution() {
-
-
-		if (jDistribution==null){
-			jDistribution = new JPanel(new FlowLayout());
-			jDistribution.add(getJlDistribution());
-			jDistribution.add(getJcDistribution());
-		}
-
-		return jDistribution;
-	}
-
-
-
-	public void setjDistribution(JPanel jDistribution) {
-		this.jDistribution = jDistribution;
-	}
 
 	protected JComboBox<String> getJcDistribution() {
 
-        distributionManager =  ProbDensFunctionManager.getUniqueInstance();
-        String[] arrayDistributions = distributionManager.getValidForTTEProbDensFunctions().stream()
-                .toArray(String[]::new);
+		parametrizedFunctionManager = ParametrizedFunctionManager.getUniqueInstance();
+		Map<String, List<String>> distributionsMap = parametrizedFunctionManager.getDistributionsMap();
+		Set<String> keySet =distributionsMap.keySet();
+		String[] distributionNames = keySet.toArray(new String[keySet.size()]);
 
 		if (jcDistribution == null){
-			jcDistribution = new JComboBox<String>(arrayDistributions);
+			jcDistribution = new JComboBox<String>(distributionNames);
 			jcDistribution.setSelectedItem(tteTablePotential.getDistributionName());
 			jcDistribution.addItemListener(this);
 		}
 
 		return jcDistribution;
+	}
+
+	private void populateJcParametrization(String selectedDistribution){
+		try {
+			parametrizedFunctionManager = ParametrizedFunctionManager.getUniqueInstance();
+			Map<String, List<String>> distributionsMap = parametrizedFunctionManager.getDistributionsMap();
+			List<String> parametrizations = distributionsMap.get(selectedDistribution);
+			jcParametrization.setModel( new DefaultComboBoxModel(new Vector<String>( parametrizations )) );
+			jcParametrization.setSelectedItem(tteTablePotential.getParametrizationName());
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	protected JLabel getJlParametrization() {
+		if (jlParametrization==null){
+			jlParametrization = new JLabel("Parametrization: ");
+		}
+		return jlParametrization;
+	}
+
+	private JComboBox<String> getJcParametrization() {
+		if (jcParametrization == null){
+			jcParametrization = new JComboBox<String>();
+			populateJcParametrization(jcDistribution.getSelectedItem().toString());
+			jcDistribution.setSelectedItem(tteTablePotential.getParametrizationName());
+			jcParametrization.addItemListener(this);
+		}
+
+		return jcParametrization;
+	}
+
+	protected JPanel getjDistributionAndParametrization() {
+		if (jDistributionAndParametrization ==null){
+			jDistributionAndParametrization = new JPanel(new FlowLayout(FlowLayout.CENTER, 10,10));
+			//Distribution label + combobox
+			JPanel jDistribution = new JPanel(new FlowLayout(FlowLayout.LEFT, 5,10));
+			jDistribution.add(getJlDistribution());
+			jDistribution.add(getJcDistribution());
+
+			//Parametrization label + combobox
+			JPanel jParametrization = new JPanel(new FlowLayout(FlowLayout.LEFT, 5,10));
+			jParametrization.add(getJlParametrization());
+			jParametrization.add(getJcParametrization());
+
+			//Ditribution + Parametrizatoin
+			jDistributionAndParametrization.add(jDistribution);
+			jDistributionAndParametrization.add(jParametrization);
+		}
+
+		return jDistributionAndParametrization;
+	}
+
+
+
+	public void setjDistributionAndParametrization(JPanel jDistributionAndParametrization) {
+		this.jDistributionAndParametrization = jDistributionAndParametrization;
 	}
 
 
@@ -124,6 +175,17 @@ public class TimeToEventTablePotentialPanel
 	protected void setJcDistribution(JComboBox<String> jcDistribution) {
 		this.jcDistribution = jcDistribution;
 	}
+
+
+//	protected JComboBox<String> getJcParametrization() {
+//
+//		tteTablePotential.getDistributionName();
+//
+//		distributionManager.getParemetersOfTTEProbDensFunction("String functionName");
+//		return jcParametrization;
+//	}
+
+
 
 	protected TableWithEventsPanel getTableWithEventsPanel() {
 		if (tableWithEventsPanel ==null) {
@@ -149,29 +211,37 @@ public class TimeToEventTablePotentialPanel
 	@Override
 	public void itemStateChanged(ItemEvent e)
 	{
+		String sDistribution = jcDistribution.getItemAt(getJcDistribution().getSelectedIndex());
+		String sParametrization = jcParametrization.getSelectedItem().toString();
 		// if the state combobox is changed
 		if (e.getSource() == jcDistribution) {
 
-			String sDistribution = jcDistribution.getItemAt(getJcDistribution().getSelectedIndex());
 			if (sDistribution.compareTo(tteTablePotential.getDistributionName()) != 0) {
-				try {
-					distribution = distributionManager.getProbDensFunctionClass(sDistribution).newInstance();
-				} catch (InstantiationException ex) {
-					ex.printStackTrace();
-				} catch (IllegalAccessException ex) {
-					ex.printStackTrace();
-				}
+				//Change parametrizations
+				populateJcParametrization(sDistribution);
 				this.remove(tableWithEventsPanel);
-				tteTablePotential.changeDistribution(sDistribution);
+				sParametrization = jcParametrization.getSelectedItem().toString();
+				tteTablePotential.changeDistribution(sDistribution,sParametrization);
 				tableWithEvents = tteTablePotential.getTableWithEvents();
 				tableWithEventsPanel = new TableWithEventsPanel(node, tteTablePotential.getTableWithEvents());
 				this.add(tableWithEventsPanel, BorderLayout.CENTER);
 				this.revalidate();
 				this.repaint();
-
-
 			}
 		}
+
+
+		//Change table
+		this.remove(tableWithEventsPanel);
+		tteTablePotential.changeDistribution(sDistribution,sParametrization);
+		tableWithEvents = tteTablePotential.getTableWithEvents();
+		tableWithEventsPanel = new TableWithEventsPanel(node, tteTablePotential.getTableWithEvents());
+		this.add(tableWithEventsPanel, BorderLayout.CENTER);
+		this.revalidate();
+		this.repaint();
+
+
+
 	}
 
 
