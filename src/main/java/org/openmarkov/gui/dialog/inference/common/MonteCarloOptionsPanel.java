afaf81c6cdddc9355fb2968483528c277718794f
Carmen María Yago Sánchez <carmen.yago@gmail.com>
2021-04-22 17:33:22 +0200

    MonteCarloOptionsPanel: this.monteCarloOptions.setExcelResultsPerSeries(resultsPerSeriesCheckBox.isSelected()) refactored to 		this.monteCarloOptions.setResultsToExcel(resultsPerSeriesCheckBox.isSelected());

--- src/main/java/org/openmarkov/gui/dialog/inference/common/MonteCarloOptionsPanel.java
+++ src/main/java/org/openmarkov/gui/dialog/inference/common/MonteCarloOptionsPanel.java
@@ -183,7 +183,7 @@ public class MonteCarloOptionsPanel extends JPanel implements ActionListener {
 	private void extractMonteCarloOptions() {
 		this.monteCarloOptions.setNumSeries(Integer.parseInt(numSeriesTextField.getText()));
 		this.monteCarloOptions.setNumSimulations(Integer.parseInt(numSimulationsTextField.getText()));
-		this.monteCarloOptions.setExcelResultsPerSeries(resultsPerSeriesCheckBox.isSelected());
+		this.monteCarloOptions.setResultsToExcel(resultsPerSeriesCheckBox.isSelected());
 		this.monteCarloOptions.setTextualLog(textualLogCheckBox.isSelected());
 		this.monteCarloOptions.setMean(meanCheckBox.isSelected());
 		this.monteCarloOptions.setTrimmedMean(trimmedMeanCheckBox.isSelected());
@@ -289,7 +289,7 @@ public class MonteCarloOptionsPanel extends JPanel implements ActionListener {
 	private JCheckBox getJCheckBoxExcelResultsPerSeries() {
 		if (resultsPerSeriesCheckBox == null) {
 			//TODO use stringDatabase
-			resultsPerSeriesCheckBox = new JCheckBox("Results Per Series (.xlsx file)",monteCarloOptions.isExcelResultsPerSeries());
+			resultsPerSeriesCheckBox = new JCheckBox("Results Per Series (.xlsx file)",monteCarloOptions.isResultsToExcel());
 
 		}
 		return resultsPerSeriesCheckBox;
