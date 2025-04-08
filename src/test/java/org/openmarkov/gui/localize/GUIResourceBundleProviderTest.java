ba698633663b0efe99accd54a79f01be47de5f62
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-04-08 12:13:05 +0200

    [Test] Added preview test for annotation_processing.

---
+++ src/test/java/org/openmarkov/gui/localize/GUIResourceBundleProviderTest.java
@@ -0,0 +1,17 @@
+package org.openmarkov.gui.localize;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GUIResourceBundleProviderTest {
+    
+    @Test
+    void testBindings() {
+        String bindingResolution = org.openmarkov.gui.localize.Localize.Add.Text.Label.stringify();
+        String databaseResolution = StringDatabase.getUniqueInstance().getString("Add.Text.Label");
+        Assertions.assertEquals(bindingResolution, databaseResolution);
+    }
+}
