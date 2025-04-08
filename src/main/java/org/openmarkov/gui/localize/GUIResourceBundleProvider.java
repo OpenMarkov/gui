90be1f723eabef99ec3629cb7225137724933a21
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-04-08 11:52:51 +0200

    [Dependencies, Refactoring] org.openmarkov.annotation_processing is now a child project of org.openmarkov.

--- src/main/java/org/openmarkov/gui/localize/GUIResourceBundleProvider.java
+++ src/main/java/org/openmarkov/gui/localize/GUIResourceBundleProvider.java
@@ -4,14 +4,12 @@ import org.jetbrains.annotations.NotNull;
 import org.openmarkov.annotation_processing.localization_bindings.BindXML;
 import org.openmarkov.gui.localize.spi.LocalizeResourcesProvider;
 
-@BindXML(filePath = {"gui/localize"})
+@BindXML(filePath = {"gui/localize/Buttons_en.xml"})
 public class GUIResourceBundleProvider implements LocalizeResourcesProvider {
-    
     
     @Override
     public @NotNull String getRootOfResources() {
         return "/gui";
     }
-    
     
 }
