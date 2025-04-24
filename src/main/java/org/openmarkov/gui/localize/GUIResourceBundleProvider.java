e36fa302013c5c0f931600bb43c294d0e2d5d307
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-04-24 02:08:26 +0200

    [Jenkins] Adapted source code to allow Jenkins to 'directories' in annotations.

--- src/main/java/org/openmarkov/gui/localize/GUIResourceBundleProvider.java
+++ src/main/java/org/openmarkov/gui/localize/GUIResourceBundleProvider.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.openmarkov.annotation_processing.localization_bindings.BindLocalizations;
 import org.openmarkov.gui.localize.spi.LocalizeResourcesProvider;
 
-@BindLocalizations(filePath = "gui/localize")
+@BindLocalizations(filePath = "gui/localize/Menus_en.xml", fileIsDirectoryChild = true)
 public class GUIResourceBundleProvider implements LocalizeResourcesProvider {
     
     @Override
