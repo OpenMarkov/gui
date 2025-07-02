d804b5eb53056e86d7889626dda7e603e3ac9f6a
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-07-02 22:26:47 +0200

    [Feature] Opened all modules to allow reflections to be used across all modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,19 @@
 import org.openmarkov.gui.localize.GUIResourceBundleProvider;
 
-module org.openmarkov.gui {
+open module org.openmarkov.gui {
     
     requires org.apache.commons.io;
-	requires org.jdom2;
-	requires org.openmarkov.core;
-	requires org.openmarkov.io;
-	requires org.openmarkov.inference;
-	requires org.apache.logging.log4j;
-	requires org.apache.poi.poi;
-	requires org.apache.poi.ooxml;
-	requires org.jfree.jfreechart;
-
-	//requires com.hexidec.ekit;
-	requires org.openmarkov.annotation_processing;
+    requires org.jdom2;
+    requires org.openmarkov.core;
+    requires org.openmarkov.io;
+    requires org.openmarkov.inference;
+    requires org.apache.logging.log4j;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires org.jfree.jfreechart;
+    
+    //requires com.hexidec.ekit;
+    requires org.openmarkov.annotation_processing;
     requires org.jetbrains.annotations;
     requires java.desktop;
     requires java.prefs;
@@ -21,23 +21,22 @@ module org.openmarkov.gui {
     
     
     exports org.openmarkov.gui.action;
-	exports org.openmarkov.gui.localize;
-	exports org.openmarkov.gui.window;
-	exports org.openmarkov.gui.dialog.inference.common;
-	exports org.openmarkov.gui.loader.element;
-	exports org.openmarkov.gui.plugin;
-	exports org.openmarkov.gui.util;
-	exports org.openmarkov.gui.dialog.common;
-	exports org.openmarkov.gui.window.edition;
-	exports org.openmarkov.gui.dialog.io;
-	exports org.openmarkov.gui.dialog.costeffectiveness;
-	exports org.openmarkov.gui.dialog.treeadd;
-	exports org.openmarkov.gui.configuration;
-	exports org.openmarkov.gui.window.mdi;
-	exports org.openmarkov.gui.window.dt;
-	
-	uses org.openmarkov.core.localize.spi.LocalizeResourcesProvider;
-	provides org.openmarkov.core.localize.spi.LocalizeResourcesProvider with GUIResourceBundleProvider;
-	
-	opens icons;
+    exports org.openmarkov.gui.localize;
+    exports org.openmarkov.gui.window;
+    exports org.openmarkov.gui.dialog.inference.common;
+    exports org.openmarkov.gui.loader.element;
+    exports org.openmarkov.gui.plugin;
+    exports org.openmarkov.gui.util;
+    exports org.openmarkov.gui.dialog.common;
+    exports org.openmarkov.gui.window.edition;
+    exports org.openmarkov.gui.dialog.io;
+    exports org.openmarkov.gui.dialog.costeffectiveness;
+    exports org.openmarkov.gui.dialog.treeadd;
+    exports org.openmarkov.gui.configuration;
+    exports org.openmarkov.gui.window.mdi;
+    exports org.openmarkov.gui.window.dt;
+    
+    uses org.openmarkov.core.localize.spi.LocalizeResourcesProvider;
+    provides org.openmarkov.core.localize.spi.LocalizeResourcesProvider with GUIResourceBundleProvider;
+    
 }
