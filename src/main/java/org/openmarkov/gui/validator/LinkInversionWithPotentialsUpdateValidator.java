6d00998a0d1a15d8f7d2602df27b2b7112423dcf
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-12-22 20:15:12 +0100

    [Bugfix] Links can now be inverted again.

--- src/main/java/org/openmarkov/gui/validator/LinkInversionWithPotentialsUpdateValidator.java
+++ src/main/java/org/openmarkov/gui/validator/LinkInversionWithPotentialsUpdateValidator.java
@@ -7,6 +7,7 @@
 
 package org.openmarkov.gui.validator;
 
+import org.openmarkov.core.action.base.linkEdits.InvertLinkEdit;
 import org.openmarkov.core.model.graph.Link;
 import org.openmarkov.core.model.network.Node;
 import org.openmarkov.core.model.network.NodeType;
@@ -31,84 +32,72 @@ import java.util.List;
  * @author Manuel Arias
  */
 public class LinkInversionWithPotentialsUpdateValidator {
-
-	/**
-	 * A link can be inverted when two conditions are met:<ol>
-	 * <li>Each potential attached to its two nodes is a TablePotential or can be projected to it.</li>
-	 * <li>The new links created do not create a cycle.</li>
-	 * </ol>
-	 * @return boolean
-	 */
-	public static boolean validate(Link<Node> link) {
-
-		boolean valid = false;
-		if (link.isDirected()) {
+    
+    /**
+     * A link can be inverted when two conditions are met:<ol>
+     * <li>Each potential attached to its two nodes is a TablePotential or can be projected to it.</li>
+     * <li>The new links created do not create a cycle.</li>
+     * </ol>
+     *
+     * @return boolean
+     */
+    public static boolean validate(Link<Node> link) {
+        
+        boolean valid = false;
+        if (link.isDirected()) {
             Node node1 = link.getFrom();
             Node node2 = link.getTo();
-			valid = validNode(node1) && validNode(node2) && validNewLinks(node1, node2);
-		}
-		return valid;
-	}
-
-	private static boolean validNewLinks(Node node1, Node node2) {
-
-		// Make a probNet copy in order to not change the original network.
-		ProbNet probNet = node1.getProbNet();
-		ProbNet newProbNet = probNet.copy();
-
-		// Remove and create the links related to arc inversion.
-		Variable variable1 = node1.getVariable();
-		Variable variable2 = node2.getVariable();
-		newProbNet.removeLink(variable1, variable2, true);
-		Node newNode1 = newProbNet.getNode(variable1);
-		Node newNode2 = newProbNet.getNode(variable2);
-		newProbNet.addLink(newNode2, newNode1, true);
-        newNode1.getParents().
-			forEach(parentsNode1 -> newProbNet.addLink(parentsNode1, newNode2, true));
-        newNode2.getParents().
-			forEach(parentsNode2 -> newProbNet.addLink(parentsNode2, newNode1, true));
-
-		// Check cycles
-		PNConstraint noCycle = new NoCycle();
-        return noCycle.isMetBy(newProbNet);
-	}
-
-	/**
-	 * A node is valid when is a chance node and it contains a valid potential type.
-	 * @return boolean
-	 */
-	private static boolean validNode(Node node) {
-
-		boolean validNode = false;
-		if (node.getNodeType() == NodeType.CHANCE) {
-			List<Potential> potentials = node.getPotentials();
-			validNode = !potentials.isEmpty() && validPotentialType(potentials.get(0));
-		}
-		return validNode;
-	}
-
-	/**
-	 * A potential is valid when can be projected to a TablePotential.
-	 * @return boolean
-	 */
-	private static boolean validPotentialType(Potential potential) {
-
-		return (!(potential instanceof AugmentedTable ||
-				potential instanceof AugmentedTablePotential ||
-				potential instanceof BinomialPotential ||
-				potential instanceof FunctionPotential ||
-				potential instanceof SameAsPrevious ||
-				potential instanceof UnivariateDistrPotential));
-	}
-
+            boolean node1Valid = validNode(node1);
+            boolean node2Valid = validNode(node2);
+            boolean newLinkValid = validNewLinks(node1, node2);
+            valid = node1Valid && node2Valid && newLinkValid;
+        }
+        return valid;
+    }
+    
+    private static boolean validNewLinks(Node node1, Node node2) {
+        return new InvertLinkEdit(node1.getProbNet(), node1.getVariable(), node2.getVariable(), true).constraintsWillBeMet();
+    }
+    
+    /**
+     * A node is valid when is a chance node and it contains a valid potential type.
+     *
+     * @return boolean
+     */
+    private static boolean validNode(Node node) {
+        
+        boolean validNode = false;
+        if (node.getNodeType() == NodeType.CHANCE) {
+            List<Potential> potentials = node.getPotentials();
+            validNode = !potentials.isEmpty() && validPotentialType(potentials.get(0));
+        }
+        return validNode;
+    }
+    
+    /**
+     * A potential is valid when can be projected to a TablePotential.
+     *
+     * @return boolean
+     */
+    private static boolean validPotentialType(Potential potential) {
+        
+        return (!(potential instanceof AugmentedTable ||
+                potential instanceof AugmentedTablePotential ||
+                potential instanceof BinomialPotential ||
+                potential instanceof FunctionPotential ||
+                potential instanceof SameAsPrevious ||
+                potential instanceof UnivariateDistrPotential));
+    }
+    
     /**
      *
      * @param node1
      * @param node2
+     *
      * @return boolean
      */
     private static boolean validPotentials(Node node1, Node node2) {
-
+        
         boolean validPotentials = false;
         if (node1.getNodeType() == NodeType.CHANCE && node2.getNodeType() == NodeType.CHANCE) {
             List<Potential> potentials1 = node1.getPotentials();
@@ -119,9 +108,9 @@ public class LinkInversionWithPotentialsUpdateValidator {
         }
         return validPotentials;
     }
-
+    
     private static boolean validPotential(Potential potential) {
-
+        
         return (!(potential instanceof AugmentedTable ||
                 potential instanceof AugmentedTablePotential ||
                 potential instanceof BinomialPotential ||
@@ -129,5 +118,5 @@ public class LinkInversionWithPotentialsUpdateValidator {
                 potential instanceof SameAsPrevious ||
                 potential instanceof UnivariateDistrPotential));
     }
-
+    
 }
