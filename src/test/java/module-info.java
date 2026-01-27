5a0450ef58b111707ef9bcb4d7d8f7e831ba04e0
Manuel Arias Calleja <ManuelAriasCalleja@gmail.com>
2026-01-27 12:56:45 +0100

    Add module-info.java for the `org.openmarkov.gui` test module, declaring its required dependencies.

---
+++ src/test/java/module-info.java
@@ -0,0 +1,10 @@
+open module org.openmarkov.gui {
+    requires org.openmarkov.gui;
+    requires org.junit.jupiter.api;
+    requires org.openmarkov.core;
+    requires org.openmarkov.io;
+    requires org.openmarkov.inference;
+    requires java.desktop;
+    requires jdk.compiler;
+    requires org.jfree.jfreechart;
+}
